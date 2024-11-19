35199c3b5bd10deda4306a8087f4933918a4488e
AeRo <pawellaszczyk2003@gmail.com>
2024-11-19 11:08:52 +0100

    feat: remove machine specific build script

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#~/librespot/build.sh cargo build --release --target mipsel-unknown-linux-musl
-
-~/Projects/librespot/build.sh cargo +nightly build --release \
-    -Z build-std=std,panic_abort \
-    -Z build-std-features=panic_immediate_abort \
-    --target mipsel-unknown-linux-musl
